b22934aaa2a3371e7586ed3eee496b9adba81fa6
chrischris292 <cbchan2@illinois.edu>
2015-04-14 11:33:50 -0500

    fixes

--- data/import_data.py
+++ data/import_data.py
@@ -15,18 +15,19 @@ def sendData(args):
     line = file.readline();
     counter = 1;
     while line!="": 
-      print counter;
-      RATING = 0;
-      print line.rstrip()
-      '''
+      line = line.split(" ");
+      interest =  str(" ".join(line[len(line)-1]));
+      subString =  str(" ".join(line[0:len(line)-1]));
       response = client.create_event(
-          event="rate",
-          entity_type="user",
+          event="$set",
+          entity_type="phrase",
           entity_id=counter,
-          properties= { "tree" : line.rstrip()}
+          properties= { "phrase" : subString,
+                        "Interest" : interest
+          }
       )
       print response
-      '''
+      print counter;
       #Read New Line
       counter = counter + 1;
       line = file.readline();
@@ -35,7 +36,7 @@ if __name__ == '__main__':
     description="Import sample data for classification engine")
   parser.add_argument('--access_key', default='invald_access_key')
   parser.add_argument('--url', default="http://localhost:7070")
-  parser.add_argument('--file', default="dev.txt")
+  parser.add_argument('--file', default="train.txt")
 
   args = parser.parse_args()
   print args
--- src/main/scala/io/prediction/opennlp/engine/DataSource.scala
+++ src/main/scala/io/prediction/opennlp/engine/DataSource.scala
@@ -1,19 +1,13 @@
 package io.prediction.opennlp.engine
 
-import java.io.File
-import java.io.FileReader
-
-import edu.stanford.nlp.trees.Tree
 import io.prediction.controller.{EmptyEvaluationInfo, EmptyParams, PDataSource}
 import io.prediction.data.storage.Storage
 import io.prediction.opennlp.engine.Interest.Interest
-
 import opennlp.maxent.BasicEventStream
 import opennlp.model.OnePassDataIndexer
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 
-import scala.collection.JavaConversions._
 import scala.util.Random
 
 class DataSource(val dsp: DataSourceParams) extends PDataSource[
@@ -25,60 +19,41 @@ class DataSource(val dsp: DataSourceParams) extends PDataSource[
   val Separator = " "
 
   override def readTraining(sc: SparkContext): TrainingData = {
-    val trainingTreeStrings = allTreeStrings(sc)
-    treeStringsToTrainingData(trainingTreeStrings)
+    val trainingTreeStrings = allPhraseandInterests(sc)
+    phraseAndInterestToTrainingData(trainingTreeStrings)
   }
 
   override def readEval(
     sc: SparkContext): Seq[(TrainingData, EmptyEvaluationInfo, RDD[(Query, Interest)])] = {
-    val shuffledTreeStrings = Random.shuffle(allTreeStrings(sc))
-    val (trainingTreeStrings, testingTreeStrings) =
-      shuffledTreeStrings.splitAt((shuffledTreeStrings.size*0.9).toInt)
+    val shuffled = Random.shuffle(allPhraseandInterests(sc))
+    val (trainingSet, testingSet) =
+      shuffled.splitAt((shuffled.size*0.9).toInt)
 
-    val trainingData = treeStringsToTrainingData(trainingTreeStrings)
+    val trainingData = phraseAndInterestToTrainingData(trainingSet)
 
-    val qna = testingTreeStrings.map { line =>
+    val qna = testingSet.map { line =>
       val lastSpace = line.lastIndexOf(Separator)
       (Query(line.substring(0, lastSpace)), Interest(line.substring(lastSpace + 1).toInt))
-    }.toSeq
+    }
 
     Seq((trainingData, EmptyParams(), sc.parallelize(qna)))
   }
 
-  private def allTreeStrings(sc: SparkContext): Seq[String] = {
-    val events = Storage.getPEvents().find(appId = dsp.appId, entityType = Some("tree"))(sc)
+  private def allPhraseandInterests(sc: SparkContext): Seq[String] = {
+    val events = Storage.getPEvents().find(appId = dsp.appId, entityType = Some("phrase"))(sc)
 
-    events.flatMap { event =>
-      val tree = event.properties.get[String]("tree")
-      treeToList(tree)
+    events.map { event =>
+      val phrase = event.properties.get[String]("phrase")
+      val Interest = event.properties.get[String]("Interest")
+
+      s"$phrase $Interest"
     }.collect().toSeq
   }
 
-  private def treeStringsToTrainingData(treeStrings: Seq[String]) = {
-    val eventStream = new BasicEventStream(new SeqDataStream(treeStrings), Separator)
+  private def phraseAndInterestToTrainingData(phraseAndInterests: Seq[String]) = {
+    val eventStream = new BasicEventStream(new SeqDataStream(phraseAndInterests), Separator)
     val dataIndexer = new OnePassDataIndexer(eventStream, dsp.cutoff)
 
     TrainingData(dataIndexer)
   }
-
-  private def treeToList(treeString: String) = {
-    val tree = Tree.valueOf(treeString)
-
-    tree.subTreeList().map { subtree =>
-      if (!subtree.isLeaf) {
-        val stringBuilder = new StringBuilder()
-
-        subtree.subTreeList().foreach { subsubtree =>
-          if (subsubtree.isLeaf)
-            stringBuilder.append(s"${subsubtree.label().value()} ")
-        }
-
-        stringBuilder.append(subtree.label().value())
-
-        stringBuilder.toString()
-      } else null
-    }.filter(_ != null).toSeq
-
-  }
 }
-
